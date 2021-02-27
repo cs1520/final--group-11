a0982958314c66157a9ee204981003af1719e9d2
Alyssa <alj81@pitt.edu>
2021-02-27 22:24:48 +0000

    testing branching

--- main.py
+++ main.py
@@ -11,5 +11,12 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/search")
+def home():
+    """Return a simple HTML page."""
+    print("Hit the route!")
+    return render_template("index.html")
+
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True) 
