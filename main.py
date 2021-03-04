eb41fd26f8c9e1b540864d08ae6df1253b09c530
DannyFujito <dtf16@pitt.edu>
2021-03-04 01:56:00 +0000

    profile + survey

--- main.py
+++ main.py
@@ -46,8 +46,13 @@ def search_results():
 def profile():
     """Return a simple HTML page."""
     print("Hit the route!")
-    return render_template("index.html")
+    return render_template("profile.html")
 
+@app.route("/survey")
+def survey():
+    """Return a simple HTML page."""
+    print("Hit the route!")
+    return render_template("survey.html")
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True) 
