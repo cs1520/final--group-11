0bcedfaed0e2f473aa0f2129797d37345d15fb60
Alyssa <alj81@pitt.edu>
2021-04-24 19:33:10 +0000

    filters

--- main.py
+++ main.py
@@ -433,4 +433,4 @@ def logout():
     return redirect("/login")
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
